c9df60a3786f91152e629c0652eee464cd7d5ee2
amandastss <amandassantos3103@gmail.com>
2026-04-09 10:41:59 -0300

    FEAT: Correção de erros

--- core/views/Produto.py
+++ core/views/Produto.py
@@ -5,7 +5,6 @@ from core.models import Produto
 from core.serializers import ProdutoSerializer
 
 
-,
 class ProdutoViewSet(ModelViewSet):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
