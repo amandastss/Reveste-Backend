f6ece985cbc93aad97d085e38af60731cdfdbe95
annaaraujoifc <annaluizaaraujo.ifcaraquari@gmail.com>
2026-08-26 14:36:35 -0300

    Atualiza core/serializers/produto.py

--- core/serializers/produto.py
+++ core/serializers/produto.py
@@ -4,13 +4,38 @@ from core.models import Produto
 
 
 class ProdutoSerializer(ModelSerializer):
-    imagem_url = SerializerMethodField()
+    imagem_url = SerializerMethodField(read_only=True)
 
     class Meta:
         model = Produto
-        fields = '__all__'
+
+        fields = [
+            'id',
+            'descricao',
+            'nome',
+            'preco',
+            'marca',
+            'condicao',
+            'imagem',
+            'imagem_url',
+            'categoria',
+            'user',
+            'disponivel',
+            'criado_em',
+        ]
+
+        read_only_fields = [
+            'id',
+            'user',
+            'disponivel',
+            'criado_em',
+            'imagem_url',
+        ]
+
         extra_kwargs = {
-            'imagem': {'required': False}
+            'imagem': {
+                'required': False
+            }
         }
 
     def get_imagem_url(self, obj):
@@ -19,6 +44,7 @@ class ProdutoSerializer(ModelSerializer):
         if obj.imagem:
             if request:
                 return request.build_absolute_uri(obj.imagem.url)
+
             return obj.imagem.url
 
         return None
