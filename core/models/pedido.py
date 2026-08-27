5a8a943160a9ca3653005ee7a472e7cd2045094b
annaraujoifc <annaluizaaraujo.ifcaraquari@gmail.com>
2026-08-27 17:28:09 -0300

    FIX: core/models/pedido.py corrigido

--- core/models/pedido.py
+++ core/models/pedido.py
@@ -1,28 +1,35 @@
+from django.conf import settings
 from django.db import models
 
 
 class Pedido(models.Model):
+
     STATUS_CHOICES = [
         ('PENDENTE', 'Pendente'),
+        ('AGUARDANDO_PAGAMENTO', 'Aguardando pagamento'),
         ('PAGO', 'Pago'),
-        ('ENVIADO', 'Enviado'),
-        ('ENTREGUE', 'Entregue'),
         ('CANCELADO', 'Cancelado'),
     ]
 
     usuario = models.ForeignKey(
-        'core.User',
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name='pedidos'
     )
 
-    data_pedido = models.DateTimeField(auto_now_add=True)
-
     status = models.CharField(
-        max_length=20,
+        max_length=30,
         choices=STATUS_CHOICES,
         default='PENDENTE'
     )
 
+    criado_em = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    atualizado_em = models.DateTimeField(
+        auto_now=True
+    )
+
     def __str__(self):
-        return f"Pedido {self.id} - {self.usuario}"
+        return f'Pedido #{self.id} - {self.status}'
